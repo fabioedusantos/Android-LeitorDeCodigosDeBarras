da1c1d920b3dd771672c4d7d0f015c56e3034897
fabioedusantos <fabioedusantos@gmail.com>
2016-09-21 19:02:49 -0300

    Arquivos reais
    Arquivos reais

---
+++ app/src/main/java/br/fabio/professor/coletor/MainActivity.java
@@ -0,0 +1,42 @@
+package br.fabio.professor.coletor;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents() == null){
+                Toast.makeText(this, "Cancelado pelo usuário", Toast.LENGTH_LONG).show();
+            } else{
+                Toast.makeText(this, "Escaneado: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void coletar(View v){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(false);
+        integrator.initiateScan();
+    }
+}
